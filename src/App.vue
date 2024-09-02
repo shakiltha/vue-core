08f18088a843c79f85d3fbf2feccdacae22e7437
shakil ahmed <shakiltha338@gmail.com>
2024-09-02 18:02:44 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+// options API
+// export default {
+//   data() {
+//     return {
+//       name: "Shakil Ahmed",
+//       status: "pendisdfsdfng",
+//       lists: ["one", "two", "three", "four"],
+//       link: "https:google.com",
+//     };
+//   },
+//   methods: {
+//     toggleStatus() {
+//       if (this.status === "active") {
+//         this.status = "pending";
+//       } else if (this.status === "pending") {
+//         this.status = "inactive";
+//       } else {
+//         this.status = "active";
+//       }
+//     },
+//   },
+// };
+
+// composition API
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const name = ref("Shakil Ahmed");
+    const status = ref("active");
+    const tasks = ref(["one", "two", "three", "four"]);
+
+    const toggleStatus = () => {
+      if (status.value === "active") {
+        status.value = "pending";
+      } else if (status.value === "pending") {
+        status.value = "inactive";
+      } else {
+        status.value = "active";
+      }
+    };
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ name }}</h2>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <h3>Lists</h3>
+  <ul>
+    <li v-for="item in tasks" key="item">{{ item }}</li>
+  </ul>
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from "vue";
+const text = ref("Edit me");
+const checked = ref(true);
+const checkedNames = ref([]);
+const picked = ref("");
+const selected = ref("");
+const multiSelected = ref([""]);
+</script>
+
+<template>
+  <h2>Text input</h2>
+  <input v-model="text" />
+  <p>{{ text }}</p>
+  <h2>Checkbox</h2>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">Checked: {{ checked }}</label>
+
+  <h2>Multi checkbox</h2>
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
+  <p>Checked names: {{ checkedNames }}</p>
+
+  <h2>Radio</h2>
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
+  <p>Picked: {{ picked }}</p>
+
+  <h4>Selecte</h4>
+  <select v-model="selected" name="" id="">
+    <option disabled value="">Please select one</option>
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select>
+
+  <p>Selected: {{ selected }}</p>
+
+  <h2>Mult Select</h2>
+  <select v-model="multiSelected" style="width: 100px" multiple name="" id="">
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select>
+  <p>multiple selected: {{ multiSelected }}</p>
+</template>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
