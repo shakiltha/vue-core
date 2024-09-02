0922862c40b0b0667aec4ae6e774fb5b3ccea904
shakil ahmed <shakiltha338@gmail.com>
2024-09-02 18:07:08 +0600

    removed options api to App2.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,20 @@
-<script>
-// options API
-// export default {
-//   data() {
-//     return {
-//       name: "Shakil Ahmed",
-//       status: "pendisdfsdfng",
-//       lists: ["one", "two", "three", "four"],
-//       link: "https:google.com",
-//     };
-//   },
-//   methods: {
-//     toggleStatus() {
-//       if (this.status === "active") {
-//         this.status = "pending";
-//       } else if (this.status === "pending") {
-//         this.status = "inactive";
-//       } else {
-//         this.status = "active";
-//       }
-//     },
-//   },
-// };
-
+<script setup>
 // composition API
+
 import { ref } from "vue";
 
-export default {
-  setup() {
-    const name = ref("Shakil Ahmed");
-    const status = ref("active");
-    const tasks = ref(["one", "two", "three", "four"]);
+const name = ref("Shakil Ahmed");
+const status = ref("active");
+const tasks = ref(["one", "two", "three", "four"]);
 
-    const toggleStatus = () => {
-      if (status.value === "active") {
-        status.value = "pending";
-      } else if (status.value === "pending") {
-        status.value = "inactive";
-      } else {
-        status.value = "active";
-      }
-    };
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    };
-  },
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
 </script>
 
@@ -56,7 +23,8 @@ export default {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
-  <h3>Lists</h3>
+
+  <h3>Tasks</h3>
   <ul>
     <li v-for="item in tasks" key="item">{{ item }}</li>
   </ul>
