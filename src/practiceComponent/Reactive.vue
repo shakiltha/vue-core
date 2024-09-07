0eaa469b03186c20c5d91850fefd184832fab550
shakil ahmed <shakiltha338@gmail.com>
2024-09-07 23:48:10 +0600

    Used reactive in the Reactive component

---
+++ src/practiceComponent/Reactive.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { reactive } from "vue";
+// reactive() won't work on primitives. it will only work on objects and arrays. the benefit using reactive is you don't have to write .value to get value. you can directly access value from caller. if you need to work on primitives use ref()
+const state = reactive({ count: 0 });
+const increment = () => {
+  state.count++;
+};
+</script>
+
+<template>
+  <h2>{{ state.count }}</h2>
+  <button class="p-2 bg-slate-400" @click="increment">Increment Count</button>
+</template>
